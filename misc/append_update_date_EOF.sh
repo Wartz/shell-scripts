dd9730a586dca417b7e3c7c58a2404223a3a3537
Joe Schlimmer <joseph.schlimmer@gmail.com>
2024-11-19 15:29:14 -0500

    Create append_update_date_EOF.sh

---
+++ misc/append_update_date_EOF.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# REGEX IS STILL THE MONARCHY
+# Joe Schlimmer
+
+regexDate="[0-9]\{4\}-[0-9]\{2\}-[0-9]\{2\}"
+
+if ! tail -n1 README.md | grep -q "$regexDate"; then
+    echo -e "\n$(date '+%Y-%m-%d')" >> README.md
+else
+    sed -i "$ s/"$regexDate"$/$(date '+%Y-%m-%d')/" README.md
+fi
